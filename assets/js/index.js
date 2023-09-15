8ef9e2ddd91fa88fea8f5a471eaa09ea9648478e
RobertAhlin <>
2023-09-15 11:43:38 +0000

    add close icon on instruction div

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,9 @@ document.getElementById('toggle-div').addEventListener('click', function () {
     var overlayDiv = document.getElementById('overlay-div');
     overlayDiv.classList.toggle('hidden-div');
 });
+
+// Toggle div on index page
+document.getElementById('close-div').addEventListener('click', function () {
+    var overlayDiv = document.getElementById('overlay-div');
+    overlayDiv.classList.toggle('hidden-div');
+});
